6ba4bf94aa4251e0ec1b3b6d2809e82159885c78
TJJJK <tengjiankai@hotmail.com>
2019-01-08 13:02:34 +0800

    2

--- hello.py
+++ hello.py
@@ -1,56 +1,37 @@
 # -*- coding: utf-8 -*-
-# 正弦信号的时域波形与频谱图
+# 连续时间实指数信号
 
-import numpy as np
-import matplotlib.pyplot as pl
-import matplotlib
-import math
-import random
- 
-row = 4
-col = 4
- 
-N = 500
-fs = 5
-n = [2*math.pi*fs*t/N for t in range(N)]    # 生成了500个介于0.0-31.35之间的点
-# print n
-axis_x = np.linspace(0,3,num=N)
- 
-#频率为5Hz的正弦信号
-x = [math.sin(i) for i in n]
-pl.subplot(221)
-pl.plot(axis_x,x)
-pl.title(u'5Hz的正弦信号')
-pl.axis('tight')
- 
-#频率为5Hz、幅值为3的正弦+噪声
-x1 = [random.gauss(0,0.5) for i in range(N)]
-xx = []
-#有没有直接两个列表对应项相加的方式？？
-for i in range(len(x)):
-    xx.append(x[i]*3 + x1[i])
- 
-pl.subplot(222)
-pl.plot(axis_x,xx)
-pl.title(u'频率为5Hz、幅值为3的正弦+噪声')
-pl.axis('tight')
- 
-#频谱绘制
-xf = np.fft.fft(x)
-xf_abs = np.fft.fftshift(abs(xf))
-axis_xf = np.linspace(-N/2,N/2-1,num=N)
-pl.subplot(223)
-pl.title(u'频率为5Hz的正弦频谱图')
-pl.plot(axis_xf,xf_abs)
-pl.axis('tight')
- 
-#频谱绘制
-xf = np.fft.fft(xx)
-xf_abs = np.fft.fftshift(abs(xf))
-pl.subplot(224)
-pl.title(u'频率为5Hz的正弦频谱图')
-pl.plot(axis_xf,xf_abs)
-pl.axis('tight')
-pl.savefig(r"temp.png")
- 
-pl.show()
+import numpy as np 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import mpl_toolkits.axisartist as axisartist
+fig = plt.figure()
+ax = axisartist.Subplot(fig, 111) 
+fig.add_axes(ax)
+ax.axis[:].set_visible(False)
+ax.axis["x"] = ax.new_floating_axis(0,-0.8)
+ax.axis["x"].set_axisline_style("->", size = 1.0)
+ax.axis["y"] = ax.new_floating_axis(1,0)
+ax.axis["y"].set_axisline_style("-|>", size = 1.0)
+ax.axis["x"].set_axis_direction("top")
+ax.axis["y"].set_axis_direction("right") 
+t = np.arange(-5,5,0.1)
+s = 0.5
+y = np.e**(s*t)
+s = -0.5
+y_1 = np.e**(s*t)
+s = 0
+y_2 = np.e**(s*t)
+plt.title(r'$x(t)=Ce^{st},s=\sigma+jw_0$')
+plt.xticks([])
+plt.yticks([])
+plt.ylim(-0.8,10)
+plt.text(0,-1.2,'0',fontdict={'size':16})
+plt.text(5.5,-1.2,'t',fontdict={'size':16})
+plt.text(4,7,r'$\sigma>0$',fontdict={'size':16})
+plt.text(-5.5,7,r'$\sigma<0$',fontdict={'size':16})
+plt.text(0,1.5,'C',fontdict={'size':16})
+plt.plot(t,y)
+plt.plot(t,y_1)
+plt.plot(t,y_2)
+plt.show()

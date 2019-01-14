e87e8ffb129e5d1a24bced73aebc3f4afb1861b9
TJJJK <tengjiankai@hotmail.com>
2019-01-14 12:32:39 +0800

    1

--- hello.py
+++ hello.py
@@ -1,136 +1,47 @@
 # -*- coding: utf-8 -*-
 # 连续时间实指数信号
 
-import numpy as np 
+# 导入模块
+import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import mpl_toolkits.axisartist as axisartist
-fig = plt.figure()
-ax = axisartist.Subplot(fig, 111) 
-fig.add_axes(ax)
-ax.axis[:].set_visible(False)
-ax.axis["x"] = ax.new_floating_axis(0,-0.8)
-ax.axis["x"].set_axisline_style("->", size = 1.0)
-ax.axis["y"] = ax.new_floating_axis(1,0)
-ax.axis["y"].set_axisline_style("-|>", size = 1.0)
+# 绘图
+fig = plt.figure()# 创建画布
+ax = axisartist.Subplot(fig, 111)# 创建绘图区对象ax
+fig.add_axes(ax)# 将绘图区对象添加到画布中
+ax.axis[:].set_visible(False)# 通过set_visible方法设置绘图区所有坐标轴隐藏
+ax.axis["x"] = ax.new_floating_axis(0,-0.8)# 添加新的坐标轴
+ax.axis["x"].set_axisline_style("->", size = 1.0)# 给x轴加上箭头
+ax.axis["y"] = ax.new_floating_axis(1,0)# 添加y轴
+ax.axis["y"].set_axisline_style("-|>", size = 1.0)#y轴加箭头
 ax.axis["x"].set_axis_direction("top")
-ax.axis["y"].set_axis_direction("right") 
-t = np.arange(-5,5,0.1)
+ax.axis["y"].set_axis_direction("right") # 设置x、y轴上刻度显示方向
+t = np.arange(-5,5,0.1)# array([-5,-4.9,-4.8,...,4.8,4.9,5])
 s = 0.5
 y = np.e**(s*t)
 s = -0.5
 y_1 = np.e**(s*t)
 s = 0
 y_2 = np.e**(s*t)
-plt.title(r'$x(t)=Ce^{st},s=\sigma+jw_0$')
+plt.title(r'$x(t)=Ce^{st},s=\sigma+jw_0$')# 设置标题（函数名称）
 plt.xticks([])
-plt.yticks([])
-plt.ylim(-0.8,10)
-plt.text(0,-1.2,'0',fontdict={'size':16})
-plt.text(5.5,-1.2,'t',fontdict={'size':16})
-plt.text(4,7,r'$\sigma>0$',fontdict={'size':16})
-plt.text(-5.5,7,r'$\sigma<0$',fontdict={'size':16})
-plt.text(0,1.5,'C',fontdict={'size':16})
-plt.plot(t,y)
-plt.plot(t,y_1)
-plt.plot(t,y_2)
+plt.yticks([])# 设置样式
+plt.ylim(-0.8,10)# 设置坐标轴范围
+plt.text(0,-1.5,'0',fontdict={'size':16})# 0点位置
+plt.text(5.5,-1.5,'t',fontdict={'size':16})# 字幕t的位置
+plt.text(4,7,r'$\sigma>0$',fontdict={'size':16})# 当sigma>0时
+plt.text(-5.5,7,r'$\sigma<0$',fontdict={'size':16})# 当sigma<0时
+plt.text(0,1.3,'C',fontdict={'size':16})# c点坐标
+plt.plot(t,y,'r')# 画图y
+plt.plot(t,y_1,'b')# 画y2
+plt.plot(t,y_2,'c')# 画y3
 plt.show()
 
-# 连续时间正弦信号基波频率与周期的关系（$w_1>w_2>w_3$）
-import numpy as np 
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib 
-font = {'family':'SimHei'} 
-matplotlib.rc('font',**font) 
-matplotlib.rcParams['axes.unicode_minus']=False
-fig = plt.figure()
-plt.subplot(3,2,1)
-ax = plt.gca() 
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none') 
-ax.xaxis.set_ticks_position('bottom') 
-ax.spines['bottom'].set_position(('data', 0))
-ax.yaxis.set_ticks_position('left') 
-ax.spines['left'].set_position(('data', 0.6)) 
-plt.xticks([2],[r'$T_1$'])
-plt.yticks([])
-plt.title(r'$x_1(t)=\cos(w_1t)$')
-x = np.arange(-3,5,0.1)
-y = np.cos(5*x)
-plt.plot(x,y,color='black')
-plt.subplot(3,2,2)
-ax = plt.gca() 
-ax.spines['right'].set_color('none') 
-ax.spines['top'].set_color('none') 
-ax.xaxis.set_ticks_position('bottom') 
-ax.spines['bottom'].set_position(('data', 0)) 
-ax.yaxis.set_ticks_position('left') 
-ax.spines['left'].set_position(('data', 0.6)) 
-plt.xticks([2],[r'$T_1$'])
-plt.yticks([])
-plt.title('$x_1(t)=\cos(w_1t)$''的傅里叶变换')
-x = np.arange(-3,5,0.1)
-y = np.cos(5*x)
-plt.plot(x,np.fft.fft(y),color='black') 
-plt.subplot(3,2,3)
-ax = plt.gca() 
-ax.spines['right'].set_color('none') 
-ax.spines['top'].set_color('none')
 
-ax.xaxis.set_ticks_position('bottom') 
-ax.spines['bottom'].set_position(('data', 0)) 
-ax.yaxis.set_ticks_position('left') 
-ax.spines['left'].set_position(('data', 0.6)) 
-plt.xticks([2],[r'$T_2$'])
-plt.yticks([])
-plt.title(r'$x_2(t)=\cos(w_2t)$')
-x = np.arange(-3,5,0.1)
-y = np.cos(3*x)
-plt.plot(x,y,color='black')
-plt.subplot(3,2,4)
-ax = plt.gca() 
-ax.spines['right'].set_color('none') 
-ax.spines['top'].set_color('none') 
-ax.xaxis.set_ticks_position('bottom') 
-ax.spines['bottom'].set_position(('data', 0))
-ax.yaxis.set_ticks_position('left') 
-ax.spines['left'].set_position(('data', 0.6)) 
-plt.xticks([2],[r'$T_2$'])
-plt.yticks([])
-plt.title('$x_2(t)=\cos(w_2t)$''的傅里叶变换')
-x = np.arange(-3,5,0.1)
-y = np.cos(3*x)
-plt.plot(x,np.fft.fft(y),color='black') 
-plt.subplot(3,2,5)
-ax = plt.gca() 
-ax.spines['right'].set_color('none') 
-ax.spines['top'].set_color('none') 
-ax.xaxis.set_ticks_position('bottom') 
-ax.spines['bottom'].set_position(('data', 0)) 
-ax.yaxis.set_ticks_position('left')
-ax.spines['left'].set_position(('data', 0.6)) 
-plt.xticks([2],[r'$T_3$'])
-plt.yticks([])
-plt.title(r'$x_3(t)=\cos(w_3t)$')
-x = np.arange(-3,5,0.1)
-y = np.cos(x)
-plt.plot(x,y,color='black')
-plt.subplot(3,2,6)
-ax = plt.gca() 
-ax.spines['right'].set_color('none') 
-ax.spines['top'].set_color('none') 
-ax.xaxis.set_ticks_position('bottom') 
-ax.spines['bottom'].set_position(('data', 0)) 
-ax.yaxis.set_ticks_position('left') ax.spines['left'].set_position(('data', 0.6)) plt.xticks([2],[r'$T_3$'])
-plt.yticks([])
-plt.title('$x_3(t)=\cos(w_3t)$''的傅里叶变换')
-x = np.arange(-3,5,0.1)
-y = np.cos(x)
-plt.plot(x,np.fft.fft(y),color='black') 
-plt.show()
+# -*- coding: utf-8 -*-
+# 单位阶跃信号及其傅里叶变换
 
-# 单位阶跃函数及其傅里叶变换
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -139,39 +50,50 @@ font = {'family':'SimHei'}
 matplotlib.rc('font',**font)
 matplotlib.rcParams['axes.unicode_minus']=False
 fig = plt.figure()
-plt.subplot(1,2,1)
+# 第一个图
+plt.subplot(1,2,1)# 画布分为一列两行，取第一个
 ax = plt.gca()
+plt.title(r'$u(t)=1$')
+# 移动脊柱
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.spines['bottom'].set_position(('data', 0))
 ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data', 0))
-t = np.arange(-10,10,0.1)
-y=0*(t<0)+1*(t>=0)
+
+t = np.arange(-10,10,0.1)# array([-10,-9.9,-9.8,...,9.8,9.9,10])
+y=0*(t<0)+1*(t>=0)# 采样信号
+# 坐标标识
 plt.text(0,-0.08,'O',fontdict={'size':16})
 plt.text(10,-0.08,'t',fontdict={'size':16})
-plt.text(0,1.5,'u(t)',fontdict={'size':16})
+plt.text(1,1.4,'u(t)',fontdict={'size':16})
+
 plt.xticks([])
 plt.yticks([])
 plt.ylim(0,1.5)
-plt.plot(t,y)
+plt.plot(t,y,'b')
+# 傅里叶变化
 plt.subplot(1,2,2)
 ax = plt.gca()
+# 移动脊柱
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.spines['bottom'].set_position(('data', -0.5))
 ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data', 0))
+# 设置画布
 plt.xticks([])
 plt.yticks([])
+
+# 坐标标识
 plt.text(0,-0.5,'O',fontdict={'size':16})
 plt.text(0.75,-0.5,r'$\omega$',fontdict={'size':16})
 plt.text(0,1,'u(t)',fontdict={'size':16})
 t = np.arange(-1,1,0.1)
-y=0*(t<0)+1*(t>=0)
+y=0*(t<0)+1*(t>=0)# 采样信号
 plt.xlim(-0.75,0.75)
 plt.ylim(-2,1)
-plt.plot(t,np.fft.fft(y))
+plt.plot(t,np.fft.fft(y),'b')# 输出傅里叶变换后信号
 plt.show()
